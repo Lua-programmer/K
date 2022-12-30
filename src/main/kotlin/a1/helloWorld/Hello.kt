49713e8d8d0a411d52c63ae082888861246375c5
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-12-29 22:32:03 -0300

    Hello World no Kotlin

---
+++ src/main/kotlin/a1/helloWorld/Hello.kt
@@ -0,0 +1,10 @@
+package a1.helloWorld // 1
+/***
+ * 1 - A especificação do pacote é opcional: se você não especificar um pacote em um arquivo, de origem, seu conteúdo irá para o pacote padrão(default).
+ * 2 - Um ponto de entrada para um aplicativo Kotlin.
+ * 3 - println grava uma linha na saída padrão. É importado implicitamente.
+ * Observe também que o ponto-e-vírgula é opcional.
+ * ***/
+fun main() { // 2
+    println("Hello World") // 3
+}
