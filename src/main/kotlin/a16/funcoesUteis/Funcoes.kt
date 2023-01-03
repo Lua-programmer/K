6611edf6af186a37c698a878678ea0e21aa21936
Luana Melissa <luanamelissaprogrammer@gmail.com>
2023-01-02 22:54:39 -0300

    map com kotlin

--- src/main/kotlin/a16/funcoesUteis/Funcoes.kt
+++ src/main/kotlin/a16/funcoesUteis/Funcoes.kt
@@ -1,6 +1,6 @@
 package a16.funcoesUteis
 
-val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11)
 
 val positives = numbers.filter { x -> x > 0 }
 
@@ -8,9 +8,18 @@ val negatives = numbers.filter { it < 0 }
 
 val pairs = numbers.filter { x -> x % 2 == 0 }
 
+//O map passa e altera a lista
+
+val doubled = numbers.map { x -> x * 2 }
+
+val tripled = numbers.map { it * 3 }
+
+
 fun main() {
-    print(positives)
-    print(negatives)
-    print(pairs)
+    println(positives)
+    println(negatives)
+    println(pairs)
+    println(doubled)
+    println(tripled)
 
 }
